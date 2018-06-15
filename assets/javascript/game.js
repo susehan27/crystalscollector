fe16a41198278cbfa62110a62fcf7ddaf39e80bd
susehan <susannahan27@gmail.com>
2018-06-15 01:37:33 -0400

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+// Each crystal is assigned to random number value
+// Random Number will generate a random number at the start of each game
+// Wins = 0, Losses = 0
+// Every time you click a crystal, it will add to the score according to assigned value
+// User wins if score === random number (alert win, +1 win count)
+// user loses if score is above random number (alert loss +1 loss count)
+// restart game
+
+
+
+var randomNumber = Math.floor(Math.random()*100+19);
+var crystalNumber = [];
+var crystals = [];
+var score = 0;
+var wins = 0;
+var losses = 0;
+
+var startGame = function() {
+    wins = 0;
+    losses = 0;
+    score = 0;
+
+    $("#randomNumber").text(randomNumber);
+    $("#score").text(score);
+    $(".crystals").each(function() {
+        crystalNumber = [Math.floor(Math.random()*10+1)];
+        crystals.push($(this).attr("data-crystalValue", crystalNumber));
+    });
+}
+
+$(".crystals").click(function(){
+    var crystalValue = ($(this).attr("data-crystalValue"));
+    crystalValue = parseInt(crystalValue);
+    score += crystalValue;
+    $("#score").text(score);
+
+    console.log(crystalValue);
+
+    if (score === randomNumber) {
+        wins++;
+        $("#wins").text(wins);
+        $(".winloss").text("WINNER");
+        startGame();
+    }
+    else if (score >= randomNumber) {
+        losses++;
+        $("#losses").text(losses);
+        $(".winloss").text("LOSER");
+        startGame();
+    }
+})
+
+
+
+    $(document).ready(function() {
+        startGame();
+    })
+
